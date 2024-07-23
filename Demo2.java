d278564384067e570c531e123653150da94d76ad
akeshrajyara <akeshrajyara0018@gmail.com>
2024-07-23 19:39:03 +0530

    modified and commited

--- Demo2.java
+++ Demo2.java
@@ -1,21 +1,30 @@
 package com.pack1;
 
 public class Demo2 {
-
-	public static void main(String[] args) {
-		for(int n = 1; n<=100; n++) {
-			int count = 0;
-			for(int i = 1 ; i <= n; i++) {
-				if(n%i==0) 
-					count++;
-			}
-			if(count == 2) {
-				System.out.println(n);
-			}
-			else
-				System.out.println("composite"+n);
+public static void main(String args[])
+{
+	int n=9;
+	if(n==1)
+	{
+		System.out.println("this is a composite number");
+	}
+	else
+	{
+		int c=0;
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+				c++;
 		}
-		
+	if(c==2)
+	{
+		System.out.println("this is a prime number");
+	}else
+	{
+		System.out.println("this is composite number");
 	}
 
+
+}
+}
 }

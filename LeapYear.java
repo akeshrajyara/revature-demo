49b9e33f0506b99c3cc39c9b66896f0afeaf9187
akeshrajyara <akeshrajyara0018@gmail.com>
2024-07-25 14:58:14 +0530

    commiting modified code

--- LeapYear.java
+++ LeapYear.java
@@ -3,8 +3,8 @@ package com.pack1;
 public class LeapYear {
 
 	public static void main(String[] args) {
-		int year = 2023;
-		if(year%400==0 || year%100!=0 && year%4==0) {
+		int year = 1700;
+		if(year%400==0 && year%100==0 && year%4==0) {
 			System.out.println(year+" is a leap year");
 		}
 		else {
